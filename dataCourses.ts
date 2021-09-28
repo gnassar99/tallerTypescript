4118f5ebfde0c8bdb3dac7f453b08262a0793e9f
gnassar99 <60226993+gnassar99@users.noreply.github.com>
2021-09-28 12:24:10 -0500

    first commit

---
+++ dataCourses.ts
@@ -0,0 +1,12 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de SW", "Jose", 4),
+  new Course("DALGO", "Jorge", 2),
+  new Course("Sistrans", "Anamaria", 2),
+  new Course("Logistica", "Alex", 1),
+  new Course("Finanzas", "Julio", 3),
+  new Course("Estrategia", "Felipe", 3),
+  new Course("Simulacion", "Carlos", 3)
+] 
+  
